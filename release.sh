a14c9be5326c9f07e7f78a2153d0302600448d5f
simonzg <zhanghan.simon@gmail.com>
2019-03-20 16:18:12 -0700

    release script

--- release.sh
+++ release.sh
@@ -1,53 +1,45 @@
 #!/bin/bash
-
+PROJECT_NAME=pow
 VERSION=1.0
 
 DOCKER_TAG=dfinlab/pow:$VERSION
 GIT_TAG=v$VERSION
-TEMP_CONTAINER_NAME=powtemp
-RELEASE_TARBALL=pow-$VERSION-linux-amd64.tar.gz
-RELEASE_DIR=release/pow-$VERSION-linux-amd64
-DEPENDENCY_TARBALL=pow-$VERSION-linux-amd64-dependency.tar.gz
-DEPENDENCY_DIR=release/pow-$VERSION-linux-amd64-dependency
+TEMP_CONTAINER_NAME=${PROJECT_NAME}-temp
+RELEASE_DIR=release/${PROJECT_NAME}-$VERSION-linux-amd64
+RELEASE_TARBALL=${PROJECT_NAME}-$VERSION-linux-amd64.tar.gz
+DEPENDENCY_TARBALL=${PROJECT_NAME}-$VERSION-linux-amd64-dependency.tar.gz
 
-docker build -t $DOCKER_TAG .
+# docker build -t $DOCKER_TAG .
 docker run -d --name $TEMP_CONTAINER_NAME $DOCKER_TAG
 echo "Brought up a temporary docker container"
-mkdir -p $RELEASE_DIR
-mkdir -p $DEPENDENCY_DIR
-docker cp $TEMP_CONTAINER_NAME:/usr/local/bin/bitcoind $RELEASE_DIR/
-docker cp $TEMP_CONTAINER_NAME:/usr/local/bin/bitcoin-cli $RELEASE_DIR/
-docker cp $TEMP_CONTAINER_NAME:/usr/local/bin/bitcoin-tx $RELEASE_DIR/
-docker cp $TEMP_CONTAINER_NAME:/usr/lib $DEPENDENCY_DIR/
+mkdir -p $RELEASE_DIR/bin
+docker cp $TEMP_CONTAINER_NAME:/usr/local/bin/bitcoind $RELEASE_DIR/bin/
+docker cp $TEMP_CONTAINER_NAME:/usr/local/bin/bitcoin-cli $RELEASE_DIR/bin/
+docker cp $TEMP_CONTAINER_NAME:/usr/local/bin/bitcoin-tx $RELEASE_DIR/bin/
+docker cp $TEMP_CONTAINER_NAME:/usr/lib $RELEASE_DIR/
 docker rm --force $TEMP_CONTAINER_NAME
 echo "Removed the temporary docker container"
 
 
-tar -zcf release/$RELEASE_TARBALL $RELEASE_DIR
-tar -zcf release/$DEPENDENCY_TARBALL $DEPENDENCY_DIR/lib
-rm -rf $RELEASE_DIR
+cd $RELEASE_DIR/bin && tar -zcf ../$RELEASE_TARBALL . && cd -
+cd $RELEASE_DIR/lib && rm -R -- */ && tar -zcf ../$DEPENDENCY_TARBALL . && cd -
+cp $RELEASE_DIR/$RELEASE_TARBALL release
+cp $RELEASE_DIR/$DEPENDENCY_TARBALL release
 
-github-release release \
-    --user dfinlab \
-    --repo btcpow \
-    --tag ${GIT_TAG} \
-    --name "${GIT_TAG}" \
-    --pre-release
+
+# rm -rf $RELEASE_DIR
+
+github-release release --user dfinlab --repo btcpow \
+    --tag ${GIT_TAG} --name "${GIT_TAG}" --pre-release
 echo "Created release ${GIT_TAG}"
 
 echo "Start upload release/${RELEASE_TARBALL}"
-github-release upload \
-    --user dfinlab \
-    --repo btcpow \
-    --tag ${GIT_TAG} \
-    --name "${RELEASE_TARBALL}" \
+github-release upload --user dfinlab --repo btcpow \
+    --tag ${GIT_TAG} --name "${RELEASE_TARBALL}" \
     --file release/$RELEASE_TARBALL
 
 echo "Start upload release/${DEPENDENCY_TARBALL}"
-github-release upload \
-    --user dfinlab \
-    --repo btcpow \
-    --tag ${GIT_TAG} \
-    --name "${DEPENDENCY_TARBALL}" \
+github-release upload --user dfinlab  --repo btcpow \
+    --tag ${GIT_TAG} --name "${DEPENDENCY_TARBALL}" \
     --file release/$DEPENDENCY_TARBALL
 echo "Release uploaded, please check https://github.com/dfinlab/btcpow/releases"
